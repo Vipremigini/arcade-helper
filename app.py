bb3d422f0a99c6c18bf1ccdf9437768ec98a3271
Aditya P Venugopal <vipremigini@gmail.com>
2024-06-22 20:25:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.post("/api/try")
+def trial():
+  return{"blocks": [
+    {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "*It's 80 degrees right now.*"
+      }
+    }]}, 200
+
