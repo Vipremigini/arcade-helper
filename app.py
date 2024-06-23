ac59eb733d661b23dacec22cbb5879b686bf682d
Aditya P Venugopal <vipremigini@gmail.com>
2024-06-23 16:14:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ app = Flask(__name__)
 
 @app.post("/api/try")
 def trial():
-    thread = threading.Thread(target=reply(request.form.get("response_url"))
+    thread = threading.Thread(target=reply(request.form.get("response_url")))
     thread.start()
 
     return {"blocks": [
